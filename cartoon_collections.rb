b9a62020dd4f1d1f3cde8ee530f59c280f8e105c
jmoypace <jmoypace@gmail.com>
2018-06-27 02:37:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  array.each_with_index{|name, index|
+  dwarves.each_with_index{|name, index|
 puts "#{index}. #{name}"
   }
 end
