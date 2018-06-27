640749b2da202616f783d2f707575214529bb956
jmoypace <jmoypace@gmail.com>
2018-06-27 02:57:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any?{|word|word.length>=4}
+  if(array.any?{|word|word.length>=4})
+  else false
+  
 end
 
 def find_the_cheese# code an argument here
