f0f1e40410b6a004721ec42294c29ee6d063f1b7
jmoypace <jmoypace@gmail.com>
2018-06-27 02:58:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  if(array.any?{|word|word.length>=4})
-  else false
+  array.any?{|word|word.length>4}
+  
 
 end
 
